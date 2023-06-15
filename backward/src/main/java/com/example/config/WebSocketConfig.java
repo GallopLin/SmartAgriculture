3068911acbe7089443bbb14679329d5d736283a2
ZhichengLin <1317864235@qq.com>
2023-06-15 11:24:35 +0800

    fix bug

--- backward/src/main/java/com/example/config/WebSocketConfig.java
+++ backward/src/main/java/com/example/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(sensorDataHandler, "/sensor-data");
+        registry.addHandler(sensorDataHandler, "/sensor-data").setAllowedOrigins("*");;
     }
 }
