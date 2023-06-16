613565fefce3cd42a33046d3d5baf9cef19a6148
ZhichengLin <1317864235@qq.com>
2023-06-16 23:27:12 +0800

    update sensor

--- Sensor/src/main/java/com/example/sensor/Sensor.java
+++ Sensor/src/main/java/com/example/sensor/Sensor.java
@@ -16,18 +16,18 @@ public class Sensor {
 
     }
 
-    public double generateTemperature(double max, double min) {
+    public double generateTemperature(double min, double max) {
         // 模拟温度传感器数据
         Random random = new Random();
         return Math.round((random.nextDouble() * (max - min) + min) * 100) / 100.0;
     }
 
     public String generateData() throws JsonProcessingException {
-        double soilMoisture = generateTemperature(20.0, 60.0);
-        double soilTemperature = generateTemperature(5.0, 30.0);
+        double soilMoisture = generateTemperature(50.0, 60.0);
+        double soilTemperature = generateTemperature(20.0, 23.0);
         double ph = generateTemperature(7.0, 7.5);
-        double airTemperature = generateTemperature(20.0, 35.0);
-        double airHumidity = generateTemperature(30.0, 60.0);
+        double airTemperature = generateTemperature(20.0, 23.0);
+        double airHumidity = generateTemperature(50.0, 60.0);
         double lightIntensity = generateTemperature(3000.0, 3100.0);
         LocalDateTime createTime = LocalDateTime.now();
         Information information = new Information(soilMoisture,soilTemperature,ph,airTemperature,airHumidity,lightIntensity,createTime);
